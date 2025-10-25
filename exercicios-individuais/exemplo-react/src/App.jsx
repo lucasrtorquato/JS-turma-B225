69a1cdf8b2f4c011aff6ccd387c2ddef38e29935
Lucas Torquato <lucas.torquato.senai@gmail.com>
2025-10-25 14:43:40 -0300

    exemplos react

--- exercicios-individuais/exemplo-react/src/App.jsx
+++ exercicios-individuais/exemplo-react/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <Titulo titulo="Teste exemplo senai" />
+        <Titulo titulo="Exemplo padrão Vite + React" />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
